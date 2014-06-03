c19b15df7840ee08846fdefdfabc86e31e1f13d6
Leon Chambers <leon8556@mit.edu>
2014-06-03 15:47:09 -0400

    Initial Commit

---
+++ src/com/example/helloglass/MainService.java
@@ -0,0 +1,43 @@
+package com.example.helloglass;
+
+import com.google.android.glass.timeline.LiveCard;
+import com.google.android.glass.timeline.LiveCard.PublishMode;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+
+public class MainService extends Service {
+
+    private static final String LIVE_CARD_TAG = "hello_glass";
+    private LiveCard mLiveCard;
+    
+	@Override
+	public IBinder onBind(Intent intent) {
+		return new Binder();
+	}
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        if (mLiveCard == null) {
+            mLiveCard = new LiveCard(this, LIVE_CARD_TAG);
+            mLiveCard.attach(this);
+            mLiveCard.publish(PublishMode.REVEAL);
+        }
+        else {
+            mLiveCard.navigate();
+        }
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        if (mLiveCard != null && mLiveCard.isPublished()) {
+            mLiveCard.unpublish();
+            mLiveCard = null;
+        }
+        super.onDestroy();
+    }
+}
+
